a1a63a8d91c6aa2406ed165a2d98f427b07e591c
Nice22 <benothouessou74@gmail.com>
2022-12-09 15:56:36 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,7 @@ int main(void)
 		for (b = a + 1; b <= 99; b++)
 		{
 			putchar((a / 10) + '0');
-			putcha((a % 10) + '0');
+			putchar((a % 10) + '0');
 			putchar(' ');
 			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
