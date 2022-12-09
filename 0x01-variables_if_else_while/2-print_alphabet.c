3598c52d5747cb824493bbe6d4997bb5a333fed5
Nice22 <benothouessou74@gmail.com>
2022-12-09 12:07:22 -0800

    I sometimes suffer from insomnia

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
-#include <stdlib.h>
-/*
- * main - main block
- * Description: Get a random number and check its last digit, compare it with 5
- * Return: 0
- */
-int main(void) 
-{
-	char c = a;
-	
-	while (c <= z)
-	{
-	putchar(c);
-	c++;
-											}
 
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	char a;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
 	putchar('\n');
 	return (0);
 }
