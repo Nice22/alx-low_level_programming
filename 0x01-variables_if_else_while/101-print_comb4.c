b16d4fb12d31707dfb91e8dfcc3e58df4fc3aab6
Nice22 <benothouessou74@gmail.com>
2022-12-09 15:29:38 -0800

    The success combination in business is

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -35,20 +35,17 @@ int main(void)
 					putchar('0' + e);
 					putchar('0' + d);
 					putchar('0' + c);
-					
+
 					if (c + d + e != 9 + 8 + 7)
 					{
 						putchar(',');
 						putchar(' ');
 					}
 				}
-				
 				c++;
 			}
-			
 			d++;
 		}
-		
 		e++;
 	}
 	putchar('\n');
