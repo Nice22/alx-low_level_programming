5a6c3f485838c7e324bd44dd3cc96b6ff94393aa
Nice22 <benothouessou74@gmail.com>
2022-12-09 15:54:06 -0800

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+*main - prints numbers
+*Return: returns 0
+*/
+int main(void)
+{
+	int a;
+	int b;
+	
+	for (a = 0; a <= 99; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putcha((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if (a == 98 && b == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}putchar('\n');
+	return (0);
+}
