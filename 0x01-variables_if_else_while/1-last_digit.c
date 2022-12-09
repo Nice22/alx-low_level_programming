bced2a04885e3df140e156e59e2a4799c8e9cbf7
Nice22 <benothouessou74@gmail.com>
2022-12-09 13:11:29 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
- * main - Entry point
- * Return: Always 0 (Success)
+/**
+ *main - Entry point
+ * 
+ * Description: 'the program's description'
+ ** 
+ *Return: Always 0 (Success)
  */
 
 int main(void)
