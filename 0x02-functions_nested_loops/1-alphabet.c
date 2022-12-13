f1f80036952aeb14e5a264e82c2386ce0bbc1a0d
Nice22 <benothouessou74@gmail.com>
2022-12-13 07:14:35 -0800

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
-/*
+/**
 *main - Entry point
-*Description: 'the program's description'
+*Description: 'the program's description' 
 *Return: Always 0 (Success)
-*void print_alphabet
 */
-
 void print_alphabet(void)
 {
 	char c = 'a';
