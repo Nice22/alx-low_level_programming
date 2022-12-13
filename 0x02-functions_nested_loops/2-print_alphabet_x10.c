7db3a8a19b3ec9f41d0830092383703d9aac1dae
Nice22 <benothouessou74@gmail.com>
2022-12-13 08:28:59 -0800

    alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,17 +1,25 @@
 #include "main.h"
 
-
+/**
+*main - Entry point
+*Description: 'the program's description'
+*Return: Always 0 (Success)
+*/
 
 void print_alphabet_x10(void)
 {
-		char c;
-			int i = 0;
-
-				while (i < 10)
-						{
-									c = 'a';
-											while (c <= 'z')
-														{
-																		_putchar(c);
-																					c++;
-																							}													_putchar('\n');i++;}}
+	char c;
+	int i = 0;
+	
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
