0731bb045f471f5e41ac95ce5b083019dcaea551
Nice22 <benothouessou74@gmail.com>
2022-12-13 11:06:31 -0800

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,11 +1,10 @@
 #include "main.h"
-/*
- * main - Entry point
- * Description: 'the program's description'
- * add: returns the sum of its parameters
- * @a: int type number
- * @b: int type number
- * Return:0
+/**
+ * add - Adds two integers.
+ * @x: first int
+ * @y: second int
+ *
+ * Return: The result of the addition.
  */
 
 
