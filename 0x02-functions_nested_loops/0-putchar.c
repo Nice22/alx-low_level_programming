28dc053dc391a9df285eba36914d33bd5d9731b3
Nice22 <benothouessou74@gmail.com>
2022-12-13 04:45:53 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/**
+*main - Entry point
+*Description: 'the program's description'
+*Return: Always 0 (Success)
+*/
+
+int main (void)
+{
+	char c[9] = "_putchar";
+	int i =0;
+	while (i < 8)
+	{
+		_putchar (c[i]);
+		i++;
+	}
+	
+	_putchar ('\n');
+	return (0);
+
+}
