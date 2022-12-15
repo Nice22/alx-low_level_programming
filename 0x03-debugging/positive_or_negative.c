59c8184d20a606e30e3b26576d36dfb03de9ae3e
Nice22 <benothouessou74@gmail.com>
2022-12-15 23:07:29 +0100

    main

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+ * positive_or_negative - Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * @i: number 
+ * Return: void
+ */
+
+void positive_or_negative(int i)
+{
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+	if (i> 0)
+		printf("%i is positive\n", i);
+	else if (i < 0)
+		printf("%i is negative\n", i);
+	else
+		printf("%i is zero\n", i);
+	return (0);
+}
