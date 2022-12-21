660767d73960326d58ac8187ca1f73b7369b9220
Nice22 <benothouessou74@gmail.com>
2022-12-22 00:06:12 +0100

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,13 +1,12 @@
 #include "main.h"
-/**
- * main - check the code
- * *_strncpy - copys a string with n
- * @dest: copy to
- * @src: copy from
- * @n: number of char to be copied
- * Return: dest
- */
 
+/*
+* _strncpy - copy a string with no
+* @dest: copy to
+* @src: copy from
+* @n: number of char to be copied
+* Return: dest 
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
