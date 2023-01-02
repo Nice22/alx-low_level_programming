e11ccac1cd72cbf845539ea685f1fb7748c97d6b
Nice22 <benothouessou74@gmail.com>
2023-01-02 07:37:17 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
 /**
- * _memcpy function copies n bytes from memory
  * _memcpy - function copies n bytes from memory area
  * src to memory area dest
  * @dest: pointer to char params
