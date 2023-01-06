23fa0c64aaac1501f1a1082d3a4b2a5355fac6ba
Nice22 <benothouessou74@gmail.com>
2023-01-06 14:46:15 +0100

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - function
+ * @argc: count
+ * @argv: value
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[argc - 1]);
+	return (0);
+}
