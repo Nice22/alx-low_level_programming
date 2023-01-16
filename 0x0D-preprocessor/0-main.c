69b862a66fff9edb7ee3d6ad1b0d4bb6f03fd1bc
Nice22 <benothouessou74@gmail.com>
2023-01-16 12:42:41 -0800

    0-main.c

--- 0x0D-preprocessor/0-main.c
+++ 0x0D-preprocessor/0-main.c
@@ -8,13 +8,14 @@
  * Return: Always 0.
  */
 /**
- *Nick
+ * Nick
  */
 int main(void)
 {
-    int s;
+	int s;
 
-    s = 98 + SIZE;
-    printf("%d\n", s);
-    return (0);
+	s = 98 + SIZE;
+	printf("%d\n", s);
+	return (0);
 }
+
