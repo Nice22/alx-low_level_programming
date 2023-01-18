eeffaf30fe99344bdc5114fcfb72377afdff97e8
Nice22 <benothouessou74@gmail.com>
2023-01-18 12:52:40 +0100

    print_name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
 #include "function_pointers.h"
 
-/*
- * print_name - print the name
+/**
+ * print_name - prints a name
  * @name: name's main
  * @f: void function
  */
 
-/*
+/**
  * Nick
  */
 
