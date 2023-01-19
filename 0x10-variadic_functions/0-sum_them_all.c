64429d2e3cb7a851ef731b359da09ef79d13ef27
Nice22 <benothouessou74@gmail.com>
2023-01-19 17:20:38 +0100

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,9 +2,10 @@
 #include <stdarg.h>
 
 /**
- * sum_them_all - sum all arguments
+ * sum_them_all -  sum all arguments
  * @n: int, number of undefined arguments
- * Returns: sum
+ *
+ * Return: sum of args
  * On error, 0 is returned
  */
 
