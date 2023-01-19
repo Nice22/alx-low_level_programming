931571e0f35d038a8a7f435841672913461f154a
Nice22 <benothouessou74@gmail.com>
2023-01-19 17:34:25 +0100

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,20 +8,20 @@
  * Return: sum of args
  * On error, 0 is returned
  */
-
 /*
  * Nick
  */
 
-int sum_them_all(const unsigned int n, ...);
+int sum_them_all(const unsigned int n, ...)
 {
-	va_list list;
-	unsigned int i, sum = 0;
+va_list list;
+unsigned int i, sum = 0;
 
-	va_start(list, n);
-	if (n != 0)
-		for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
-			;
-	va_end(list);
-	return (sum);
+va_start(list, n);
+if (n != 0)
+	for (i = 0; i < n; sum += va_arg(list, unsigned int), i++)
+	;
+va_end(list);
+
+return (sum);
 }
