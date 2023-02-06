9b306091391b3fbabe6d63d104117624271815dc
Nice22 <benothouessou74@gmail.com>
2023-02-06 15:54:57 +0100

    function that creates a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,8 +8,8 @@
  * Return: w- actual number of bytes read and printed
  * 0 when function fails or filename is NULL.
  */
-
 /* Nick2219 */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buf;
