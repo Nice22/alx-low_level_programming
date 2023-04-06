ac32be47067f08c0d9bf8033ea18cf6a04238c7d
Nice22 <benothouessou74@gmail.com>
2023-04-06 09:39:55 -0700

    a hash function implementing the djb2 algorithm

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+/* Nice22 */
+
+/**
+ * hash_djb2 - hash function generates a djb2
+ * @str: pointer to chars a hash
+ * Return: hash key
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long hash = 5381;
+	int q;
+
+	while ((q = *str++))
+	hash = ((hash << 5) + hash) + q; /* hash * 33 + q */
+
+	return (hash);
+}
