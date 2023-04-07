6a01834af8a17415680c8beb59f25ae331600ac6
Nice22 <benothouessou74@gmail.com>
2023-04-07 16:02:13 -0700

    6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -8,20 +8,20 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i;
+	unsigned long int j;
 	hash_node_t *next;
 
 	if (ht == NULL || ht->array == NULL || ht->size == 0)
 		return;
-	for (i = 0; i < ht->size; i++)
+	for (j = 0; j < ht->size; j++)
 	{
-		while (ht->array[i] != NULL)
+		while (ht->array[j] != NULL)
 		{
-			next = ht->array[i]->next;
-			free(ht->array[i]->key);
-			free(ht->array[i]->value);
-			free(ht->array[i]);
-			ht->array[i] = next;
+			next = ht->array[j]->next;
+			free(ht->array[j]->key);
+			free(ht->array[j]->value);
+			free(ht->array[j]);
+			ht->array[j] = next;
 		}
 	}
 	free(ht->array);
